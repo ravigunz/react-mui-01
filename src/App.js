42803a9d4cd3f03b471d0bf5c1dacce2b9a0e531
ravigunz <ravigunz@gmail.com>
2023-05-13 23:37:50 +0530

    install mui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BasicButtons from './layouts/BasicButtons';
+
+function App() {
+  return (
+    <div>
+      <h1>Material UI Components</h1>
+      <BasicButtons />
+    </div>
+  );
+}
+
+export default App;
